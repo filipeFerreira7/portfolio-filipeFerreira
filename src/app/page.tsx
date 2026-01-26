a83043b8176cd44e3c65b9f0525b4c5104124fc3
Filipe Ferreira <filipeferreira.psid2019@gmail.com>
2026-01-26 13:52:44 -0300

    testando combinação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 
-export const dynamic = 'force-dynamic';
+//export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
 import {
   ChevronDown,
   Github,
